84d82b4d75e924c391b17c62e32c7ed20fa52233
Heber Uchytil <heberespinoza2200@gmail.com>
2025-05-17 17:52:12 -0600

    gallery

---
+++ Cool Pics/coolpics.js
@@ -0,0 +1,48 @@
+const menuButton = document.querySelector(".menu-button");
+function toggleMenu() {
+    const menu = document.querySelector(".menu");
+    menu.classList.toggle("hide");
+}
+menuButton.addEventListener("click", toggleMenu);
+
+function handleResize() {
+    const menu = document.querySelector(".menu");
+    if (window.innerWidth > 1000) {
+        menu.classList.remove("hide");
+    } else {
+        menu.classList.add("hide");
+    }
+}
+
+handleResize();
+window.addEventListener("resize", handleResize);
+
+const gallery = document.querySelector(".gallery"); 
+gallery.addEventListener("click", (event) => {
+    const modal = document.querySelector(".image-viewer");
+    if (modal && event.target === modal) {
+        modal.close();
+    }
+});
+
+document.querySelector(".gallery").addEventListener("click", (event) => {
+    if (event.target.tagName === "IMG") {
+        const modal = document.createElement("dialog");
+        modal.classList.add("image-viewer");
+        modal.innerHTML = `<img src="${event.target.src}" alt=""><button class="close-viewer">X</button>`;
+        document.body.append(modal);
+        modal.showModal();
+
+        modal.querySelector(".close-viewer").addEventListener("click", () => {
+            modal.close();
+            modal.remove();
+        });
+
+        modal.addEventListener("click", (e) => {
+            if (e.target === modal) {
+                modal.close();
+                modal.remove();
+            }
+        });
+    }
+});
